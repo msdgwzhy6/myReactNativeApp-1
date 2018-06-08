120a8c35633518640e65f34df31bbbf24e320226
youngjuning <1003719811@qq.com>
2018-06-08 14:43:32 +0800

    增加注释

--- App.js
+++ App.js
@@ -1,58 +1,35 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
+  View,
   Text,
-  View
+  Image,
+  StyleSheet,
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class LotsOfStyle extends Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to youngjuning's React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+         <Text style={[styles.bigBlue,styles.red]}>just red</Text>
+         <Text style={[styles.bigBlue]}>React Native Text</Text>
+         <Image source={pic} style={{width: 193, height: 110}} />
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'pink'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  red: {
+    color: 'red',
+  }
 });
