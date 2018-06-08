56c676563f707497acbe64d8c6bb9865e9a6c27e
youngjuning <1003719811@qq.com>
2018-06-08 17:37:35 +0800

    网络请求

--- App.js
+++ App.js
@@ -2,22 +2,34 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  SectionList,
   StyleSheet,
 } from 'react-native';
 
-export default class SectionListBasics extends Component {
+export default class Fetch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: '电影',
+    };
+  }
+  async getMoviesFromApiAsync() {
+    try {
+      let response = await fetch('https://cnodejs.org/api/v1/topics');
+      let responseJson = await JSON.stringify(response);
+      return responseJson;
+    } catch (e) {
+      console.error(error);
+    }
+  }
   render() {
+    this.getMoviesFromApiAsync().then(res => {
+      this.setState(previousState => {
+        return {movie: res}
+      })
+    })
     return (
       <View style={styles.container}>
-        <SectionList
-          sections={[
-            {title: 'D', data: ['Devin']},
-            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
-          ]}
-          renderItem={({item}) => <Text style={[styles.item]}>{item}</Text>}
-          renderSectionHeader={({section}) => <Text style={[styles.sectionHeader]}>{section.title}</Text>}
-        />
+        <Text>{this.state.movie}</Text>
       </View>
     );
   }
@@ -26,20 +38,5 @@ export default class SectionListBasics extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 22,
   },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingRight: 10,
-    paddingBottom: 2,
-    paddingLeft: 10,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  }
 });
