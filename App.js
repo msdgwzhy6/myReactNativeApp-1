4df5a9c37553ea3a1aa09645b63ab0ef99a6f1cf
杨俊宁 <1003719811@qq.com>
2018-06-10 14:46:41 +0800

    动态列表展示

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  FlatList,
   StyleSheet,
 } from 'react-native';
 
@@ -9,27 +10,37 @@ export default class Fetch extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movie: '电影',
+      topics: '电影',
     };
   }
   async getMoviesFromApiAsync() {
     try {
       let response = await fetch('https://cnodejs.org/api/v1/topics');
-      let responseJson = await JSON.stringify(response);
-      return responseJson;
+      let responseJson = await response.json();
+      return responseJson.data;
     } catch (e) {
       console.error(error);
     }
   }
-  render() {
+  //在生命周期中执行该function
+  componentWillMount() {
     this.getMoviesFromApiAsync().then(res => {
-      this.setState(previousState => {
-        return {movie: res}
+      this.setState({
+        topics: res
       })
-    })
+    });
+  }
+  render() {
     return (
-      <View style={styles.container}>
-        <Text>{this.state.movie}</Text>
+      <View style={[styles.container]}>
+        <FlatList
+          // 接收一个数组
+          data={this.state.topics}
+          // item渲染器
+          renderItem={({item, index}) => <Text style={styles.item}>{index+1}、{item.title}</Text>}
+          // key提取器
+          keyExtractor = { (item, index) => index.toString()}
+        />
       </View>
     );
   }
@@ -37,6 +48,11 @@ export default class Fetch extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
+  },
+  item: {
+    paddingBottom: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
   },
 });
