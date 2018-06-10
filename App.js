6b1f49a4306770534a7f4a4d00406344e9f4c335
杨俊宁 <1003719811@qq.com>
2018-06-10 18:55:37 +0800

    更新一些组件

--- App.js
+++ App.js
@@ -3,36 +3,63 @@ import {
   View,
   Text,
   FlatList,
+  Alert,
+  Button,
+  CheckBox,
+  ActivityIndicator,
   StyleSheet,
 } from 'react-native';
+
+const onPressLearnMore = () => {
+  Alert.alert('Button has been pressed!');
+}
+
+const getMoviesFromApiAsync = async() =>{
+  try {
+    let response = await fetch('https://cnodejs.org/api/v1/topics');
+    let responseJson = await response.json();
+    return responseJson.data;
+  } catch (e) {
+    console.error(error);
+  }
+}
 
 export default class Fetch extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      checkBoxValue: false,
+      animating: true,
       topics: '电影',
     };
   }
-  async getMoviesFromApiAsync() {
-    try {
-      let response = await fetch('https://cnodejs.org/api/v1/topics');
-      let responseJson = await response.json();
-      return responseJson.data;
-    } catch (e) {
-      console.error(error);
-    }
-  }
+
   //在生命周期中执行该function
   componentWillMount() {
-    this.getMoviesFromApiAsync().then(res => {
+    getMoviesFromApiAsync().then(res => {
       this.setState({
         topics: res
       })
     });
   }
+
   render() {
     return (
       <View style={[styles.container]}>
+        <Button
+          onPress={onPressLearnMore}
+          title="Learn More"
+          color="yellowgreen"
+        />
+        <CheckBox
+          value={this.state.checkBoxValue}
+          onValueChange={(prevState) => {
+            console.log(this.state.checkBoxValue);
+            this.setState({
+              checkBoxValue: !this.state.checkBoxValue
+            })
+          }}
+        />
         <FlatList
           // 接收一个数组
           data={this.state.topics}
@@ -40,6 +67,11 @@ export default class Fetch extends Component {
           renderItem={({item, index}) => <Text style={styles.item}>{index+1}、{item.title}</Text>}
           // key提取器
           keyExtractor = { (item, index) => index.toString()}
+        />
+        <ActivityIndicator
+          size="large"
+          animating = {this.state.animating}
+          style={[styles.centering, {backgroundColor: '#eeeeee'}]}
         />
       </View>
     );
