4f74e2ef83e4e1f1e71f7e335623ced74a209415
youngjuning <1003719811@qq.com>
2018-06-11 17:42:40 +0800

    使用ESLint格式化js

--- LeanCloud/Article.js
+++ LeanCloud/Article.js
@@ -1,22 +1,22 @@
 import AV from 'leancloud-storage/live-query'
 
 export default class Article extends AV.Object {
-  constructor () {
+  constructor() {
     // 调用 父类构造函数，并注册
     super()
     AV.Object.register(Article)
   }
 
   // 保存文章
-  static async saveArticle(jsonParams,id) {
+  static async saveArticle(jsonParams, id) {
     let article = null
     if (id) {
-      article = AV.Object.createWithoutData('Article',id)
+      article = AV.Object.createWithoutData('Article', id)
     } else {
       article = new AV.Object('Article')
     }
     try {
-      let savedObj = await article.save(jsonParams)
+      const savedObj = await article.save(jsonParams)
       return savedObj
     } catch (err) {
       return err
@@ -24,10 +24,10 @@ export default class Article extends AV.Object {
   }
 
   static async getArticles() {
-    let query = new AV.Query('Article')
+    const query = new AV.Query('Article')
     try {
-      let result = await query.find()
-      let data = []
+      const result = await query.find()
+      const data = []
       result.forEach((item) => {
         data.push(item.toJSON())
       })
