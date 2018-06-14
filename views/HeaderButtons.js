4cf422b4081bee0ef74d2d0b5449eb0c6f977c5c
youngjuning <1003719811@qq.com>
2018-06-14 14:45:39 +0800

    完结图标

--- views/HeaderButtons.js
+++ views/HeaderButtons.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import {
-  View,
+  ScrollView,
   Text,
+  Alert,
   StyleSheet,
 } from 'react-native'
 
@@ -12,8 +13,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  btns: {
-    flexDirection: 'row',
+  icon: {
+    marginTop: 5,
+    marginRight: 'auto',
+    marginBottom: 5,
+    marginLeft: 'auto',
   },
 })
 
@@ -26,8 +30,8 @@ export default class HeaderButton extends Component {
         iconSize={23}
         color="#fff"
       >
-        <HeaderButtons.Item title="select" iconName="edit" />
-        <HeaderButtons.Item title="select" iconName="search" />
+        <HeaderButtons.Item title="select" iconName="edit" onPress={() => Alert.alert('我是顶部编辑按钮')} />
+        <HeaderButtons.Item title="select" iconName="search" onPress={() => Alert.alert('我是顶部搜索按钮')} />
       </HeaderButtons>
     ),
   })
@@ -37,10 +41,65 @@ export default class HeaderButton extends Component {
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Icon name="rocket" size={30} color="#900" />
-        <Text>I am the HeaderButton component</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <Icon name="rocket" size={20} color="pink" style={[styles.icon]}>
+          <Text>rocket</Text>
+        </Icon>
+        <Icon name="bell" size={20} color="pink" style={[styles.icon]}>
+          <Text>bell</Text>
+        </Icon>
+        <Icon name="battery-three-quarters" size={20} color="pink" style={[styles.icon]}>
+          <Text>battery-three-quarters</Text>
+        </Icon>
+        <Icon name="user-circle-o" size={20} color="pink" style={[styles.icon]}>
+          <Text>user-circle-o</Text>
+        </Icon>
+        <Icon name="wifi" size={20} color="pink" style={[styles.icon]}>
+          <Text>wifi</Text>
+        </Icon>
+        <Icon name="user-circle" size={20} color="pink" style={[styles.icon]}>
+          <Text>user-circle</Text>
+        </Icon>
+        <Icon name="weixin" size={20} color="pink" style={[styles.icon]}>
+          <Text>weixin</Text>
+        </Icon>
+        <Icon name="vk" size={20} color="pink" style={[styles.icon]}>
+          <Text>vk</Text>
+        </Icon>
+        <Icon name="weibo" size={20} color="pink" style={[styles.icon]}>
+          <Text>weibo</Text>
+        </Icon>
+        <Icon name="windows" size={20} color="pink" style={[styles.icon]}>
+          <Text>windows</Text>
+        </Icon>
+        <Icon name="safari" size={20} color="pink" style={[styles.icon]}>
+          <Text>safari</Text>
+        </Icon>
+        <Icon name="github" size={20} color="pink" style={[styles.icon]}>
+          <Text>github</Text>
+        </Icon>
+        <Icon name="github-alt" size={20} color="pink" style={[styles.icon]}>
+          <Text>github-alt</Text>
+        </Icon>
+        <Icon name="github-square" size={20} color="pink" style={[styles.icon]}>
+          <Text>github-square</Text>
+        </Icon>
+        <Icon name="git" size={20} color="pink" style={[styles.icon]}>
+          <Text>git</Text>
+        </Icon>
+        <Icon name="git-square" size={20} color="pink" style={[styles.icon]}>
+          <Text>git-square</Text>
+        </Icon>
+        <Icon name="gitlab" size={20} color="pink" style={[styles.icon]}>
+          <Text>gitlab</Text>
+        </Icon>
+        <Icon name="chrome" size={20} color="pink" style={[styles.icon]}>
+          <Text>chrome</Text>
+        </Icon>
+        <Icon name="firefox" size={20} color="pink" style={[styles.icon]}>
+          <Text>firefox</Text>
+        </Icon>
+      </ScrollView>
     )
   }
 }
