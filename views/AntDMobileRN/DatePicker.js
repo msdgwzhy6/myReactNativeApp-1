1e0c2df31f607a4feb7a31ac03536b229da662a0
youngjuning <1003719811@qq.com>
2018-06-18 18:26:26 +0800

    完善 DateTime 组件的demo

--- views/AntDMobileRN/DatePicker.js
+++ views/AntDMobileRN/DatePicker.js
@@ -19,18 +19,35 @@ export default class DatePickerScreen extends Component {
     super(props)
     this.state = {}
   }
-  componentDidMount() {}
   render() {
     return (
       <View style={styles.container}>
-        <DatePicker
-          minDate={new Date(1970, 1, 1)}
-          value={this.state.date}
-          title="SelectDate"
-          onChange={date => this.setState({ date })}
-        >
-          <List.Item arrow="horizontal">Datetime</List.Item>
-        </DatePicker>
+        <List>
+          <DatePicker
+            mode="date"
+            value={this.state.date}
+            minDate={new Date(2015, 7, 6)}
+            maxDate={new Date(2026, 11, 3)}
+            onChange={date => this.setState({ date })}
+          >
+            <List.Item arrow="horizontal">Select Date</List.Item>
+          </DatePicker>
+          <DatePicker
+            mode="time"
+            value={this.state.time}
+            onChange={time => this.setState({ time })}
+          >
+            <List.Item arrow="horizontal">Select Time</List.Item>
+          </DatePicker>
+          <DatePicker
+            mode="datetime"
+            value={this.state.datetime}
+            minDate={new Date(1970, 1, 1)}
+            onChange={datetime => this.setState({ datetime })}
+          >
+            <List.Item arrow="horizontal">Select Datetime</List.Item>
+          </DatePicker>
+        </List>
       </View>
     )
   }
