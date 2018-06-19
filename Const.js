5826c63b54efdf2622ed52c6aaaa082f3066fcad
youngjuning <1003719811@qq.com>
2018-06-19 09:49:36 +0800

    发到github的时候，不需要发布正式包

---
+++ Const.js
@@ -0,0 +1,14 @@
+export const FacePlusPlus = {
+  API_KEY: 'uumiIymfdx3SyqulvaHIKgltKTqGc6qh',
+  API_SECRET: 'ZXgWOIOiLhBr3vPRekIkhbmF8DglrgE_',
+  BaseApi: 'https://api-cn.faceplusplus.com/facepp/v3/',
+}
+export const Leancloud = {
+  APP_ID: 'qY4TYxDE1JFCnqfxgFsQ6Duv-gzGzoHsz',
+  APP_KEY: '04NSJrvc2cl82hXUaioeFANx',
+}
+
+export default {
+  Leancloud,
+  FacePlusPlus,
+}
