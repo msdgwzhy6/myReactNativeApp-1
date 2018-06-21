abf1f401c75c2b00d46451b56550807b0e3d3ddd
youngjuning <1003719811@qq.com>
2018-06-21 17:10:05 +0800

    teaset 作为备选ui框架，主力使用 antd-mobile-rn

--- App-teaset.js
+++ App-teaset.js
@@ -4,19 +4,9 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 import {
   createBottomTabNavigator,
   createStackNavigator,
-  createMaterialTopTabNavigator,
 } from 'react-navigation'
 import AV from 'leancloud-storage/live-query'
 import { Leancloud } from './assets/Dictionary'
-// Home
-import Home from './views/Home'
-import ArticleList from './views/ArticleList'
-import Login from './views/Login'
-import HeaderButtons from './views/HeaderButtons'
-import Face from './views/Face++/Face'
-// AntDMobileRN
-import AntDMobileRN from './views/AntDMobileRN'
-import AntDDatePicker from './views/AntDMobileRN/DatePicker'
 // Teaset
 import Teaset from './views/Teaset'
 import ActionPopover from './views/Teaset/ActionPopover'
@@ -34,16 +24,6 @@ import PopoverPicker from './views/Teaset/PopoverPicker'
 import SearchInput from './views/Teaset/SearchInput'
 import Select from './views/Teaset/Select'
 import Stepper from './views/Teaset/Stepper'
-// Plugins
-import ReactNativePicker from './views/Plugins/ReactNativePicker'
-import Plugins from './views/Plugins'
-import ReactNativeModalDatetimePicker from './views/Plugins/ReactNativeModalDatetimePicker'
-import ReactNativeCalendars from './views/Plugins/ReactNativeCalendars'
-import ReactNativeAmap3d from './views/Plugins/ReactNativeAmap3d'
-import GiftedChat from './views/Plugins/GiftedChat'
-// Charts
-import echarts from './views/Charts/Echarts'
-import secharts from './views/Charts/SEcharts'
 
 // 初始化leancloud
 AV.init({
@@ -53,39 +33,7 @@ AV.init({
 // 设置teaset主题
 Theme.set(Theme.themes.poetry)
 
-const HomeStack = createStackNavigator(
-  {
-    Home,
-    Login,
-    HeaderButtons,
-    ArticleList,
-    Face,
-  },
-  {
-    initialRouteName: 'Home',
-    navigationOptions: {
-      headerStyle: {
-        backgroundColor: Theme.primaryColor,
-      },
-      headerTintColor: 'rgb(255,255,255)',
-    },
-  },
-)
-const AntDMobileRNStack = createStackNavigator(
-  {
-    AntDMobileRN,
-    AntDDatePicker,
-  },
-  {
-    initialRouteName: 'AntDMobileRN',
-    navigationOptions: {
-      headerStyle: {
-        backgroundColor: Theme.primaryColor,
-      },
-      headerTintColor: 'rgb(255,255,255)',
-    },
-  },
-)
+// Teaset页面栈
 const TeasetStack = createStackNavigator(
   {
     Teaset, ActionPopover, ActionSheet, AlbumView, Badge, Carousel, Drawer, Checkbox, ListRow, Menu, Popover, PullPicker, PopoverPicker, SearchInput, Select, Stepper,
@@ -99,32 +47,9 @@ const TeasetStack = createStackNavigator(
     },
   },
 )
-const PluginsStack = createStackNavigator(
-  {
-    Plugins, ReactNativePicker, ReactNativeModalDatetimePicker, ReactNativeCalendars, ReactNativeAmap3d, GiftedChat,
-  },
-  {
-    navigationOptions: {
-      headerStyle: {
-        backgroundColor: Theme.primaryColor,
-      },
-      headerTintColor: 'rgb(255,255,255)',
-    },
-  },
-)
-const ChartsStack = createMaterialTopTabNavigator(
-  {
-    echarts,
-    secharts,
-  },
-  {
-    initialRouteName: 'secharts',
-    navigationOptions: {
-      header: null,
-    },
-  },
-)
-const Stacks = [HomeStack, AntDMobileRNStack, TeasetStack, PluginsStack, ChartsStack]
+
+// 指定页面不显示header
+const Stacks = [TeasetStack]
 Stacks.forEach((item) => {
   item.navigationOptions = ({ navigation }) => {
     let tabBarVisible = true
@@ -137,34 +62,25 @@ Stacks.forEach((item) => {
   }
 })
 
+// 构建 BottomTabNavigator 并导出
 export default createBottomTabNavigator(
   {
-    Home: HomeStack,
-    AntDMobile: AntDMobileRNStack,
     Teaset: TeasetStack,
-    Plugins: PluginsStack,
-    Charts: ChartsStack,
   },
   {
-    initialRouteName: 'Plugins',
+    initialRouteName: 'Teaset',
     tabBarOptions: {
       activeTintColor: Theme.primaryColor ? Theme.primaryColor : 'tomato',
       inactiveTintColor: 'gray',
     },
     navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, tintColor }) => {
+      tabBarIcon: ({ tintColor }) => {
         const { routeName } = navigation.state
         let iconName
         if (routeName === 'Home') {
           iconName = 'home'
-        } else if (routeName === 'AntDMobile') {
-          iconName = 'heart'
         } else if (routeName === 'Teaset') {
           iconName = 'thumbs-up'
-        } else if (routeName === 'Plugins') {
-          iconName = 'th-list'
-        } else if (routeName === 'Charts') {
-          iconName = 'area-chart'
         }
         return <Icon name={iconName} size={25} color={tintColor} />
       },
