6fd09665ab7c4f3d75cb2731a6c4924f546f25f7
杨俊宁 <1003719811@qq.com>
2018-06-25 08:05:34 +0800

    完成 pickerView

--- views/AntDMobileRN/PickerView.js
+++ views/AntDMobileRN/PickerView.js
@@ -1,27 +1,60 @@
 import React, { Component } from 'react'
-import {
-  View,
-  Text,
-  StyleSheet,
-} from 'react-native'
+import { PickerView } from 'antd-mobile-rn'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-})
-
-export default class MyComponent extends Component {
+const seasons = [
+  [
+    {
+      label: '2013',
+      value: '2013',
+    },
+    {
+      label: '2014',
+      value: '2014',
+    },
+  ],
+  [
+    {
+      label: '春',
+      value: '春',
+    },
+    {
+      label: '夏',
+      value: '夏',
+    },
+    {
+      label: '秋',
+      value: '秋',
+    },
+    {
+      label: '冬',
+      value: '冬',
+    },
+  ],
+]
+export default class PickerViewScreen extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('title', 'PickerView'),
+  })
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      value: null,
+    }
   }
-  componentDidMount() {}
+  onChange = (value) => {
+    this.setState({
+      value,
+    })
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>I am the MyComponent component</Text>
-      </View>
+      <PickerView
+        onChange={this.onChange}
+        value={this.state.value}
+        data={seasons}
+        cols={2}
+        cascade={false}
+      />
     )
   }
 }
