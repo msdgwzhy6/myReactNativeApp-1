395d23d6147962d315ebaba202bcabd59c700570
youngjuning <1003719811@qq.com>
2018-06-25 11:41:37 +0800

    完成 搜索框

--- views/AntDMobileRN/SearchBar.js
+++ views/AntDMobileRN/SearchBar.js
@@ -1,9 +1,13 @@
+// 规则
+// 1. 应该在 placeholder 里提供提示文字，提醒用户输入相关内容，比如：双11特卖。
+// 2. 在搜索栏下方，可提供有用的标签文案，帮助用户通过点击直接完成输入，比如：列出一些最近搜索的关键词。
 import React, { Component } from 'react'
 import {
   View,
-  Text,
   StyleSheet,
+  Alert,
 } from 'react-native'
+import { SearchBar } from 'antd-mobile-rn'
 
 const styles = StyleSheet.create({
   container: {
@@ -14,12 +18,29 @@ const styles = StyleSheet.create({
 export default class SearchScreen extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      value: '美食',
+    }
+  }
+  onChange = (value: any) => {
+    this.setState({ value })
+  }
+
+  clear = () => {
+    this.setState({ value: '' })
   }
   render() {
     return (
       <View style={styles.container}>
-        <Text>I am the SearchScreen component</Text>
+        <SearchBar defaultValue="初始值" placeholder="搜索" />
+        <SearchBar
+          value={this.state.value}
+          placeholder="搜索"
+          onSubmit={(value: any) => Alert.alert(value)}
+          onCancel={this.clear}
+          onChange={this.onChange}
+          showCancelButton
+        />
       </View>
     )
   }
