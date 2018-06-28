ba767130ebbd47c80e113a390fa4f423cac66f3f
youngjuning <1003719811@qq.com>
2018-06-28 10:07:52 +0800

    删除一些注释，修改入口

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import { YellowBox, AppRegistry } from 'react-native'
 // import TeasetApp from './App-teaset'
 import AntdApp from './App-antd'
 
-// YellowBox
 YellowBox.ignoreWarnings([
   'Warning: isMounted(...) is deprecated',
   'Module RCTImageLoader',
