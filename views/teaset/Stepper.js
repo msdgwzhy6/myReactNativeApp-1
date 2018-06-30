7a7d9c8d899ed10d18a9de9d018646426e69881a
杨俊宁 <1003719811@qq.com>
2018-07-01 01:03:26 +0800

    airbnb 17.0.0

--- views/teaset/Stepper.js
+++ views/teaset/Stepper.js
@@ -17,11 +17,14 @@ export default class StepperScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: navigation.getParam('title', 'Stepper'),
   })
+
   constructor(props) {
     super(props)
     this.state = {}
   }
+
   componentDidMount() {}
+
   render() {
     return (
       <View style={styles.container}>
@@ -34,24 +37,28 @@ export default class StepperScreen extends Component {
         <ListRow
           title="苹果"
           titlePlace="left"
-          detail={<Stepper
-            defaultValue={0}
-            min={0}
-            max={10}
-            onChange={value => Alert.alert(`${value}`)}
-          />}
+          detail={(
+            <Stepper
+              defaultValue={0}
+              min={0}
+              max={10}
+              onChange={value => Alert.alert(`${value}`)}
+            />
+        )}
           topSeparator="full"
           bottomSeparator="full"
         />
         <ListRow
           title="香蕉"
           titlePlace="left"
-          detail={<Stepper
-            defaultValue={0.8}
-            step={0.005}
-            valueFormat={v => `${(v * 100).toFixed(1)}%`}
-            valueStyle={{ minWidth: 60 }}
-          />}
+          detail={(
+            <Stepper
+              defaultValue={0.8}
+              step={0.005}
+              valueFormat={v => `${(v * 100).toFixed(1)}%`}
+              valueStyle={{ minWidth: 60 }}
+            />
+          )}
           topSeparator="full"
           bottomSeparator="full"
         />
