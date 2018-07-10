67d084554ffcf3906077234edf472b0f7467ce5f
youngjuning <1003719811@qq.com>
2018-07-10 10:28:21 +0800

    调整首页的样式

--- views/Home.js
+++ views/Home.js
@@ -5,7 +5,10 @@ import {
 } from 'react-native'
 import { Button } from 'teaset'
 import SplashScreen from 'react-native-splash-screen'
+import { List } from 'antd-mobile-rn'
 
+const Item = List.Item
+const Brief = List.Item.Brief
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -36,26 +39,30 @@ export default class Home extends Component {
     const { navigation } = this.props
     return (
       <ScrollView style={[styles.container]}>
-        <Button
-          style={[styles.btn]}
-          size="lg"
-          title="登录"
-          onPress={() => navigation.navigate('Login', { msg: '来自首页的消息' })}
-        />
-        <Button
-          style={[styles.btn]}
-          type="secondary"
-          size="lg"
-          title="ArticleList"
-          onPress={() => navigation.navigate('ArticleList')}
-        />
-        <Button
-          style={[styles.btn]}
-          type="danger"
-          size="lg"
-          title="Face++"
-          onPress={() => navigation.navigate('Face')}
-        />
+
+        <List>
+          <Item
+            arrow="horizontal"
+            onClick={() => navigation.navigate('Login', { msg: '来自首页的消息' })}
+          >
+            登录
+            <Brief>基于LeanCloud的登录页面</Brief>
+          </Item>
+          <Item
+            arrow="horizontal"
+            onClick={() => navigation.navigate('ArticleList')}
+          >
+            ArticleList
+            <Brief>基于LeanCloud的文章列表</Brief>
+          </Item>
+          <Item
+            arrow="horizontal"
+            onClick={() => navigation.navigate('Face')}
+          >
+            Face++
+            <Brief>基于Face++的人脸识别</Brief>
+          </Item>
+        </List>
       </ScrollView>
     )
   }
